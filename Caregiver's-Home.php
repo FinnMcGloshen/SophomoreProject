51b480edf3d45c0ed3dd360bc2e7225dcda3945f
FinnMcGloshen <famcgloshen211@stevenscollege.edu>
2022-12-02 10:07:58 -0500

    idk

--- Caregiver's-Home.php
+++ Caregiver's-Home.php
@@ -59,7 +59,7 @@
           </a>
         </li>
         <li>
-          <a href="Patients.html">
+          <a href="patients.php">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"

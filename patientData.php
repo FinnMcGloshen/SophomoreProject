0eca7920eab5200d8dbbb533fc00f18ee200d9ab
awork211 <work.aaron89@gmail.com>
2022-12-02 09:58:57 -0500

    added admission date

--- patientData.php
+++ patientData.php
@@ -2,12 +2,14 @@
         $myObj = array();
         $dbconn = pg_connect("host=localhost dbname=aaronwork user=aaronwork password=gamecube")
         or die('Could not connect: ' . pg_last_error());
-        $result = pg_query($dbconn, "SELECT id, name FROM patients;");
+        $result = pg_query($dbconn, "SELECT id, fname, lname, admission_date FROM accounts WHERE role = 'Patient';");
         while ($row = pg_fetch_row($result)) {
             // row[0] - id, row[1] - name
-            $myObj[$row[0]] = $row[1];
+            $myObj[$row[0]] = array($row[1]." ".$row[2], $row[3]);
+            
         }
         $myJSON = json_encode($myObj);
         echo $myJSON;
+
         pg_close($dbconn);
 ?>

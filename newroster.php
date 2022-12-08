bc2184887a4d39bd4beeddc1c9bb1ce8ca50e89a
awork211 <work.aaron89@gmail.com>
2022-12-08 08:55:21 -0500

    roster page php

--- newroster.php
+++ newroster.php
@@ -1,108 +1,38 @@
-<html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roster
-    </title>
-    <style>
-        table,th,td{
-            border:1px solid black
-        }
-    </style>
-</head>
-<body>
-    <div class="newroster">
-        <h1>New Roster</h1>
-        <p>Date</p>
-        <input type="date" class="date" placeholder="Date"/>
-        <br>
-        <div>
-        <p>Supervisor: </p>
-        <select name="Supervisor" id="supervisor">
-        <option value="Supervisor">Dan Miller</option>
-        </select>
-        </div>
-        <div>
-        <p>Doctor:</p> 
-        <select name="Doctor" id="doctor">
-        <option value="Doctor">Christine Philips</option>
-        </select>
-        </div>
-        
-        <div>
-        <p>Caregiver 1:</p>
-        <select name="Caregiver 1" id="caregiver">
-        <option value="Caregiver">Barbara Stevens</option>
-        <option value="Caregiver">Steve Johnson</option>
-        <option value="Caregiver">Craig Morgan</option>
-        <option value="Caregiver">Abby Watson</option>
-        </select>
-        <p>Patient Group:</p>
-        <select name="cg2 patient group">
-            <option value="patient group">A</option>
-            <option value="patient group">B</option>
-            <option value="patient group">C</option>
-            <option value="patient group">D</option>
-        </select>
-        </div>
-        
-        <div>
-        <p>Caregiver 2:</p>
-        <select name="Caregiver 2" id="caregiver">
-        <option value="Caregiver">Barbara Stevens</option>
-        <option value="Caregiver">Steve Johnson</option>
-        <option value="Caregiver">Craig Morgan</option>
-        <option value="Caregiver">Abby Watson</option>
-        </select>
-        <p>Patient Group:</p>
-        <select name="cg2 patient group">
-            <option value="patient group">A</option>
-            <option value="patient group">B</option>
-            <option value="patient group">C</option>
-            <option value="patient group">D</option>
-        </select>
-        </div>
-        
-        <div>
-        <p>Caregiver 3:</p>
-        <select name="Caregiver 3" id="caregiver">
-        <option value="Caregiver">Barbara Stevens</option>
-        <option value="Caregiver">Steve Johnson</option>
-        <option value="Caregiver">Craig Morgan</option>
-        <option value="Caregiver">Abby Watson</option>
-        </select>
-        <p>Patient Group:</p>
-        <select name="cg2 patient group">
-            <option value="patient group">A</option>
-            <option value="patient group">B</option>
-            <option value="patient group">C</option>
-            <option value="patient group">D</option>
-        </select>
-        </div>
-        
-        <div>
-        <p>Caregiver 4:</p>
-        <select name="Caregiver 4" id="caregiver">
-        <option value="Caregiver">Barbara Stevens</option>
-        <option value="Caregiver">Steve Johnson</option>
-        <option value="Caregiver">Craig Morgan</option>
-        <option value="Caregiver">Abby Watson</option>
-        </select>
-        <p>Patient Group:</p>
-        <select name="cg2 patient group">
-            <option value="patient group">A</option>
-            <option value="patient group">B</option>
-            <option value="patient group">C</option>
-            <option value="patient group">D</option>
-        </select>
-    </div>
-    <br>
-    <button>OK</button>
-    <button>Cancel</button>
-    </div>
-</body>
-</html>
-</html>
+<?php
+$dbconn = pg_connect("host=localhost dbname=aaronwork user=aaronwork password=gamecube")
+or die('Could not connect: ' . pg_last_error());
+
+// initialize data variables
+$dataObj = array();
+$dataObj['supervisors'] = array();
+$dataObj['doctors'] = array();
+$dataObj['caregivers'] = array();
+
+// queries by role
+$superSearch = pg_query($dbconn, "SELECT fname, lname FROM accounts WHERE role = 'Supervisor';");
+$doctorSearch = pg_query($dbconn, "SELECT fname, lname FROM accounts WHERE role = 'Doctor';");
+$caregiverSearch = pg_query($dbconn, "SELECT fname, lname FROM accounts WHERE role = 'Caregiver';");
+
+
+// pushing results to arrays
+while ($row = pg_fetch_row($caregiverSearch)){
+    array_push($dataObj['caregivers'], $row[0]." ".$row[1]);
+};
+
+while ($row = pg_fetch_row($doctorSearch)){
+    array_push($dataObj['doctors'], $row[0]." ".$row[1]);
+};
+
+while ($row = pg_fetch_row($superSearch)){
+    array_push($dataObj['supervisors'], $row[0]." ".$row[1]);
+};
+
+// echo var_dump($dataObj['doctors']);
+// echo var_dump($dataObj['caregivers']);
+// echo var_dump($dataObj['supervisors']);
+
+$myJSON = json_encode($dataObj);
+echo $myJSON;
+
+pg_close($dbconn);
+?>

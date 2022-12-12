f689fd46e3689c87bafa5e28eab11c71d59c2a3b
awork211 <work.aaron89@gmail.com>
2022-12-12 09:21:03 -0500

    roster php

--- roster.php
+++ roster.php
@@ -1,6 +1,4 @@
-<html>
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,13 +15,77 @@
             border:1px solid black
         }
     </style>
+    <script>
+        var returnData;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var myObj = JSON.parse(this.responseText);
+                // store data in global var
+                returnData = myObj;
+                console.log(returnData)
+            }
+        };
+        xmlhttp.open("GET", "rosterData.php", true);
+        xmlhttp.send(returnData);
+
+        // populate select with for loop
+        
+        document.addEventListener('readystatechange', (e) => {
+            if (document.readyState == 'complete'){
+                var table = document.getElementsByTagName('table')[0];
+                var keys = Object.keys(returnData)
+                var selectElem = document.getElementById('select_name');
+                // populate select element with options
+                for (var i = 0; i < keys.length; i++){
+                    var opt = document.createElement('option')
+                    opt.value = String(keys[i])
+                    opt.text = String(keys[i])
+                    selectElem.appendChild(opt)
+                }
+                var line = document.getElementById('line')
+                // listen for select change
+
+                selectElem.addEventListener('change', (e) => {
+                    tableDisplay(selectElem);
+                });
+                
+                // Display date/table data
+                function tableDisplay(selectElem){
+                    table.style.visibility = 'visible';
+                    var tableData = document.getElementsByClassName('query_data');
+                    var dateHead = document.getElementById('date_head');
+                    dateVal = selectElem.value;
+                    dateHead.innerHTML = 'Date: ' + dateVal;
+                    // change table data
+                    tableData[0].innerHTML = dateVal;
+                    tableData[1].innerHTML = returnData[String(dateVal)]['supervisor'];
+                    tableData[2].innerHTML = returnData[String(dateVal)]['doctor'];
+                    tableData[3].innerHTML = returnData[String(dateVal)]['cg1'];
+                    tableData[4].innerHTML = returnData[String(dateVal)]['cg2'];
+                    tableData[5].innerHTML = returnData[String(dateVal)]['cg3'];
+                    tableData[6].innerHTML = returnData[String(dateVal)]['cg4'];
+
+                    tableData[7].innerHTML = returnData[String(dateVal)]['cg1_group'];
+                    tableData[8].innerHTML = returnData[String(dateVal)]['cg2_group'];
+                    tableData[9].innerHTML = returnData[String(dateVal)]['cg3_group'];
+                    tableData[10].innerHTML = returnData[String(dateVal)]['cg4_group'];
+                }
+            }
+        });
+        // select the select value, and use returnData[] to display data in table
+        // var selectElem = document.getElementById('select_name');
+        
+        // console.log(selectElem)
+    </script>
 </head>
 <body>
-<div class="Btn-Fix">
+    <div class="Btn-Fix">
       <button onclick="history.go(-1);" class="Redirect-Home"> <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z"/>
       </svg>Go Back</a></button>
     </div>
+    
     <div class="roster">
     <h2 class="Welcome-Greeting-1">
       Welcome to the Millstream Village Roster!
@@ -31,55 +93,46 @@
     <br />
     <h2 class="Welcome-Greeting-2">Current Roster:</h2>
     <br>
-        <h2 class="Welcome-Greeting-2">Date: <?php echo date("Y-m-d")?></h2>
+        <select name="select_date" id="select_name">
+        <option>Select a date</option>
+        </select>
+        <h2 id="date_head" class="Welcome-Greeting-2">Date:</h2>
         <br>
         <div class="Main-Table">
-        <table>
-          <tbody>
-            <tr class="table-title">
-              <th class="table-data">Supervisor</th>
-              <th class="table-data">Doctor</th>
-              <th class="table-data">Caregiver #1</th>
-              <th class="table-data">Caregiver #2</th>
-              <th class="table-data">Caregiver #3</th>
-              <th class="table-data">Caregiver #4</th>
-            </tr>
-    </div>
-        <?php 
-        $dbconn = pg_connect("host=localhost dbname=aaronwork user=aaronwork password=gamecube")
-        or die('Could not connect: ' . pg_last_error());
-    
-    // Performing SQL query
-    // $query1 = 'DROP TABLE IF EXISTS roster;';
-    // $query2 = 'CREATE TABLE roster (roster_date date, datesupervisor text, doctor text, cg1 text, cg2 text, cg3 text, cg4 text);';
-    // $query3 = "INSERT INTO roster(supervisor,doctor,cg1,cg2,cg3,cg4)
-    // VALUES ('Dan Miller', 'Christine Philips', 'Barbara Stevens', 'Steve Johnson', 'Craig Morgan', 'Abby Watson' ),
-    // (NULL, NULL, 'Patient Group A', 'Patient Group B', 'Patient Group C', 'Patient Group D' );";
-    $query4 = 'SELECT * FROM roster;';
-    // $result1 = pg_query($query1) or die('Query failed: ' . pg_last_error());
-    // $result2 = pg_query($query2) or die('Query failed: ' . pg_last_error());
-    // $result3 = pg_query($query3) or die('Query failed: ' . pg_last_error());
-    $result4 = pg_query($query4) or die('Query failed: ' . pg_last_error());
-    
-    // Printing results in HTML
-    // echo "<table>\n";
-    while ($line = pg_fetch_array($result4, null, PGSQL_ASSOC)) {
-        echo "\t<tr class='table-title'>\n";
-        foreach ($line as $col_value) {
-            echo "\t\t<td  class='table-data'>$col_value</td>\n";
-        }
-        echo "\t</tr>\n";
-    }
-    echo "</table>\n";
-    
-    // Free resultset
-    pg_free_result($result4);
-    
-    // Closing connection
-    pg_close($dbconn);
-        
-        ?>
-    </div>
+            <table style="visibility: hidden">
+                <thead>
+                    <tr class="table-title">
+                        <th class="table-data">Date</th>
+                        <th class="table-data">Supervisor</th>
+                        <th class="table-data">Doctor</th>
+                        <th class="table-data">Caregiver #1</th>
+                        <th class="table-data">Caregiver #2</th>
+                        <th class="table-data">Caregiver #3</th>
+                        <th class="table-data">Caregiver #4</th>
+                    </tr> 
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="query_data"></td>
+                        <td class="query_data"></td>
+                        <td class="query_data"></td>
+                        <td class="query_data"></td>
+                        <td class="query_data"></td>
+                        <td class="query_data"></td>
+                        <td class="query_data"></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td class="query_data">Patient Group </td>
+                        <td class="query_data">Patient Group </td>
+                        <td class="query_data">Patient Group </td>
+                        <td class="query_data">Patient Group </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
 </body>
-</html>
 </html>
